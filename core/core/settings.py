3b82e8c2ecae2723e1e9dff568e8e799019ae554
Сайденов Рамзан <rama050102@gmail.com>
2024-01-27 17:06:40 +0600

    создал приложение order_management

--- core/core/settings.py
+++ core/core/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'rest_framework',
+    
+    'order_management',
 ]
 
 MIDDLEWARE = [
@@ -77,8 +79,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'alpacaDB',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres', 
+        'HOST': 'postgres',         
+        'PORT': '5432',
     }
 }
 
