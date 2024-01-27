3bcdf7f80c68eb2012b9da9a88f156655c3fd8a1
Сайденов Рамзан <rama050102@gmail.com>
2024-01-27 17:25:15 +0600

    добавил модель ордера и создал .env добавив зачения

--- core/core/settings.py
+++ core/core/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,11 +85,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'alpacaDB',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres', 
-        'HOST': 'postgres',         
-        'PORT': '5432',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
--- core/order_management/models.py
+++ core/order_management/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    id = models.UUIDField(primary_key=True)
+    client_order_id = models.UUIDField()
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+    submitted_at = models.DateTimeField()
+    filled_at = models.DateTimeField(null=True, blank=True)
+    expired_at = models.DateTimeField(null=True, blank=True)
+    canceled_at = models.DateTimeField(null=True, blank=True)
+    failed_at = models.DateTimeField(null=True, blank=True)
+    replaced_at = models.DateTimeField(null=True, blank=True)
+    replaced_by = models.UUIDField(null=True, blank=True)
+    replaces = models.UUIDField(null=True, blank=True)
+    asset_id = models.UUIDField()
+    symbol = models.CharField(max_length=20)
+    asset_class = models.CharField(max_length=20)
+    notional = models.DecimalField(max_digits=20, decimal_places=4, null=True, blank=True)
+    qty = models.IntegerField()
+    filled_qty = models.IntegerField()
+    filled_avg_price = models.DecimalField(max_digits=20, decimal_places=4, null=True, blank=True)
+    order_class = models.CharField(max_length=20)
+    order_type = models.CharField(max_length=20)
+    type = models.CharField(max_length=20)
+    side = models.CharField(max_length=20)
+    time_in_force = models.CharField(max_length=20)
+    limit_price = models.DecimalField(max_digits=20, decimal_places=4, null=True, blank=True)
+    stop_price = models.DecimalField(max_digits=20, decimal_places=4, null=True, blank=True)
+    status = models.CharField(max_length=20)
+    extended_hours = models.BooleanField()
+    legs = models.JSONField(null=True, blank=True)
+    trail_percent = models.DecimalField(max_digits=10, decimal_places=4, null=True, blank=True)
+    trail_price = models.DecimalField(max_digits=20, decimal_places=4, null=True, blank=True)
+    hwm = models.DecimalField(max_digits=20, decimal_places=4, null=True, blank=True)
+    commission = models.DecimalField(max_digits=20, decimal_places=4)
+    subtag = models.CharField(max_length=100)
+    source = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return f"Order {self.id}"
